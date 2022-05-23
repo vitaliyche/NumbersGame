30351baae9d18bdca37ee5e7afbc75a1ffcc357a
vitaliyche <vit.chernyavsky@gmail.com>
2022-05-23 10:18:30 +0300

    Layouts

--- app/src/main/java/com/codeliner/numbersgame/presentation/MainActivity.kt
+++ app/src/main/java/com/codeliner/numbersgame/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.codeliner.numbersgame
+package com.codeliner.numbersgame.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codeliner.numbersgame.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
